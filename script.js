624f5a671c903e62eb65a16bf9e5c7eb0592fd6e
Jim Doyle <indellibleink@gmail.com>
2018-05-15 21:59:28 -0400

    Added jquery lines

---
+++ script.js
@@ -0,0 +1,8 @@
+/* element selectors */
+$("h3").css({border: "3px solid blue"});
+
+/* class selector */
+$(".wrapper").css({border: "3px solid red"});
+
+/* ID selectors */
+$('#clients').css({border: '3px solid yellow'});
