79f0edb4eeb4431b0f98283eae000bf6c947c952
Jim Doyle <indellibleink@gmail.com>
2018-05-15 22:40:10 -0400

    Updated code for lesson 4,5,6

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-/* element selectors */
-$("h3").css({border: "3px solid blue"});
 
-/* class selector */
-$(".wrapper").css({border: "3px solid red"});
+/* Lesson 5 */
 
-/* ID selectors */
-$('#clients').css({border: '3px solid yellow'});
+/* element selectors 
+$("h3").css({border: "3px solid blue"}); */
+
+/* class selector 
+$(".wrapper").css({border: "3px solid red"}); */
+
+/* ID selectors 
+$('#clients').css({border: '3px solid yellow'}); */
+
+/* Lesson 6 */
+
+/* first and last 
+$('header nav li:first').css({border: '2px solid red'});
+$('header nav li:last').css({border: '2px solid red'}); */
+
+/* first-child and last-child 
+$('#contact ul:first').css({border: '2px solid red'});
+$('#contact ul:last').css({border: '2px solid red'});  */
+
+/* even 
+$('header nav li:even').css({border: '2px solid blue'}); */
+
+/* odd 
+$('header nav li:odd').css({border: '2px solid yellow'}); */
+
+/* not 
+$("section:not('#contact')").css({border: '2px solid green'}); */
+
+/* less than 
+$('#social-nav li:lt(3)').css({border: '2px solid blue'}); */ 
+
+/* greater than 
+$('#social-nav li:gt(2)').css({border: '2px solid blue'}); */
+
+/* attribute filters 
+$('div[class]').css({border: '2px solid pink'}); */
+
+/* attribute with specific value
+$('img[alt=quote]').css({border: '2px solid purple'}); */
