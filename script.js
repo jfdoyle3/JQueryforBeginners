a3dbab7e06db319f6b94a2c32379865f666c91a1
Jim Doyle <indellibleink@gmail.com>
2018-05-17 22:44:40 -0400

    Updated code for lesson 19,20,21

--- script.js
+++ script.js
@@ -1,66 +1,53 @@
-/* -------------------------- */
-/* Lesson 16 - Event Helpers */
-/* ------------------------ */
-
-/*
-$("#lead-banner").on("dblclick", function(){
-    alert("You double clicked me");
-    $("#lead-banner").off("dblclick");
-});
-*/
-
-/*
-on() - binds an event to matched elements(s)
-off() - unbindes event from matched element(s)
-*/
-
-
-/* ------------------------------------------- */
-/* Lesson 17 -  Document Ready vs Window Load */
-/* ----------------------------------------- */
-
-/* Few different methods for Doucment Ready */
-/*
-$(document).on("ready",function(){});
-*/
-/*
-$(doucment).ready(function(){});
-*/
-/*
-$(function(){});
-*/
-
-/* Window.load */
-/*
-$(window).on("load", function(){});
-*/
-/*
-$(window).load(function(){});
-*/
-
-
-/* --------------------------------------- */
-/* Lesson 18 - The Event Object in JQuery */
-/* ------------------------------------- */
+/* ------------------------------ */
+/* Lesson 19 - JQuery Animations */
+/* ---------------------------- */
 
 /*
 $(document).ready(function(){
-    $("*").on("click", function(e){
-        console.log(e.target);
-        console.log("The evnet Type is: "+ e.type);
-        console.log("X co-ordinate of the event is: " + e.pageX);
-        console.log("Y co-ordinate of the event is: " + e.pageY);
-        e.stopPropagation();
+    function complete(){
+        alert("animation complete")
+    };
+    $("section > h2").on("click", function(){
+        $(this).animate({"width": "500px", "height" : "100px"},1000, "linear", complete);
     });
-
 });
 */
 
+/* ----------------------------- */
+/* Lesson 20 -  Fading Elements */
+/* --------------------------- */
 
+/*
+$(document).ready(function(){
+    $("section > h2").on("click", function(){
+        $(this).fadeOut(2000).fadeIn(500); 
+        $(this).fadeTo(200, 0.2)
+               .fadeTo(200, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(200, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(200, 0.8)
+    });
+});
+*/
 
+/*
+ fadeIn(), fadeOut(), fadeTo()
+*/
 
+/* ------------------------------- */
+/* Lesson 21 - Show, Hide, Toggle */
+/* ------------------------------*/
 
+$(document).ready(function(){
+    $("img[alt=map]").on("click", function(){
+      /*  $(this).hide(1000).show(1000); */
+      $("section >h2").toggle(1000);
+        
+    });
+});
 
-
-
+/*
+hide(), show(), toggle()
+*/
 
