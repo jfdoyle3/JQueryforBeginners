4a79bcc4250d5d3919136e0247d1c3c986113ad7
Jim Doyle <indellibleink@gmail.com>
2018-05-19 00:48:12 -0400

    Updated code and comments fr lesson 25,26

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+/* -------------------------- */
+/* Lesson 25 - Using Plugins */
+/* -------------------------*/
+
+/*
+$(document).ready(function(){
+    $(".rslides").responsiveSlides();
+});
+*/
+
+/* -------------------------- */
+/* Lesson 26 - What's Next?  */
+/* ------------------------ */
+
+/* jQuery UI:
+improvemnets to user interaction:
+ideas:
+-Datapickers
+-Draggable elements
+-Sortable elements
+-Resizable elements
+
+-------------------------------------------------------------
+jQuery w/AJAX
+Asynchronous Javascript and XML  (AJAX)
+- Communicate with a server and load data into a page /o refreshing
+
+Exsmples:
+ Google Maps
+ Traffic Updates
+
+ ---------------------------------------------------------------
+
+ jQuery 
